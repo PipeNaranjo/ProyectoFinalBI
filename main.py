f55acd666b67f11a63804f055d65d1ccb2cda9b6
Sara9805 <saraariasq.98@gmail.com>
2020-11-29 17:06:17 +0000

    Version final del script que realiza las consultas a la API y las carga en el bucket asignado

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ client = boto3.client('s3', aws_access_key_id = access_key, aws_secret_access_ke
 
 url = 'https://pokeapi.co/api/v2/'
 dir = '/home/ubuntu/PokeApi/'
-#endpoints = ['berry', 'berry-firmness', 'berry-flavor','contest-type','contest-effect','super-contest-effect','encounter-method','encounter-condition','encounter-condition-value',
-#             'evolution-chain','evolution-trigger','generation','pokedex','version','version-group','item','item-category','item-fling-effect',
-#             'item-pocket','location','location-area','pal-park-area','region','machine','move','move-ailment','move-category','move-learn-method',
-#             'move-target','ability','characteristic','egg-group','gender','growth-rate','nature','pokeathlon-stat','pokemon','pokemon-color','pokemon-form',
-#             'pokemon-habitat','pokemon-shape','stat','type']
+
+endpoints = ['berry', 'berry-firmness', 'berry-flavor','contest-type','contest-effect','super-contest-effect','encounter-method','encounter-condition','encounter-condition-value',
+             'evolution-chain','evolution-trigger','generation','pokedex','version','version-group','item','item-category','item-fling-effect',
+             'item-pocket','location','location-area','pal-park-area','region','machine','move','move-ailment','move-category','move-learn-method',
+             'move-target','ability','characteristic','egg-group','gender','growth-rate','nature','pokeathlon-stat','pokemon','pokemon-color','pokemon-form',
+             'pokemon-habitat','pokemon-shape','stat','type']
 
 def cargarEndpoint(endpoint,dir1,url2):
 	respone = requests.get(url2)
@@ -70,7 +71,7 @@ def cargarItems(items):
 			lista.append(nueva)
 	return lista
 
-endpoints = ['item']
+
 for endpoint in endpoints:
 	dir1 = dir + endpoint
 	url2 = url + endpoint
